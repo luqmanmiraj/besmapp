6a0531d04287e5a9567088af7933e2d071bf95df
luqman miraj <luqmanmiraj42@gmail.com>
2019-06-13 20:17:18 +0500

    code refactored

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const port = 3000
 //const func =  require('./callbackfunction.js');
 //console.log(func.functionsssff);
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const mongoConnect = require('./util/database');
 
 
@@ -13,5 +15,5 @@ const mongoConnect = require('./util/database');
 
 mongoConnect(client => {
     console.log(client);
-    app.listen(3000);
+    app.listen(port);
   });
